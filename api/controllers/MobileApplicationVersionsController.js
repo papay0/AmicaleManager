99765bd11edcc9c40f5762c24d0612548b342227
Arthur Papailhau <papailhau@gmail.com>
2016-06-02 01:00:48 +0200

    Add message on versions not allowed mobile App

--- api/controllers/MobileApplicationVersionsController.js
+++ api/controllers/MobileApplicationVersionsController.js
@@ -12,6 +12,7 @@ module.exports = {
 		var objANDROID = new Object();
 		obj.iOS = objIOS;
 		objIOS.versionsNotAllowed = ["1.1.1", "1.1.2"];
+		objIOS.message = "La version actuelle de l'application risque de ne pas fonctionner correctement, veuillez la mettre à jour.";
 		obj.Android = objANDROID;
 		var jsonToSend = {'json':obj}
 		res.send(jsonToSend);
